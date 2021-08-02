5b84db0525ebfc0c9b865181f5392c8fa9fc5110
rileybazan115 <80065065+rileybazan115@users.noreply.github.com>
2021-08-02 17:00:40 -0600

    First Day

---
+++ Game/Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <SDL.h>
+
+int main(int, char**)
+{
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{ 
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("GAT150", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+	if (window == nullptr)
+	{
+		std::cout << "SDL_CreateWindowError:" << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+	std::getchar();
+	SDL_Quit();
+	return 0;
+}
